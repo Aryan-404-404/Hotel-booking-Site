c1c19b0b26953447b1be32428f319adaeeeeade0
Rishabh Singh <singhrishabh87799@gmail.com>
2025-03-27 23:32:31 +0530

    connected the frontend registration page with the backend

--- Backend_Development/config/connectDB.js
+++ Backend_Development/config/connectDB.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
-const connectDb = async()=>{
+const connectDB = async() => {
     try{
+        console.log("Attempting to connect to the database...")
         const connect = await mongoose.connect(process.env.CONNECTION_STRING);
-        console.log("Database connected!");
-    }
-    catch(err){
-        console.log(err);
+        console.log("Database connected : ", connect.connection.host , connect.connection.name);
+    }catch(err){
+        console.error("error conneting database : " , err.message);
         process.exit(1);
     }
 }
 
-module.exports = connectDb;
+module.exports = connectDB;
