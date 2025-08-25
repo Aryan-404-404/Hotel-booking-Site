72f548ca90cd20213f354387a28e6bd5afe25e5f
Aryan-404-404 <aryan411770@gmail.com>
2025-08-25 20:29:41 +0530

    updated error message in validate token middleware

--- Backend_Development/middlewares/validateToken.js
+++ Backend_Development/middlewares/validateToken.js
@@ -5,13 +5,13 @@ const validateToken = asyncHandler( async(req, res, next)=>{
     const authHeader = req.headers.authorization || req.headers.Authorization;
     if(!authHeader && !authHeader.startsWith("Bearer")){
         res.status(401);
-        throw new Error("User is not authorized")
+        throw new Error("User is not authorized! Please Sign-in first!")
     }
     const token = authHeader.split(" ")[1];
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded)=>{
         if(err){
             res.status(400);
-            throw new Error("User is not authorized")
+            throw new Error("User is not authorized!  Please Sign-in first!")
         }
         req.user = decoded.user;
         next();
